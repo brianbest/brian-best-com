cfeb972b10be685733c148be7f584bd711600d46
Brian Best <brian.brianb64@gmail.com>
2023-01-16 16:10:12 -0500

    image fix

--- src/ProfileImage.jsx
+++ src/ProfileImage.jsx
@@ -3,13 +3,14 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import imgUrl from './static/me.jpg';
 
 export default function ProfileImage() {
     return(
         <Card className='photo'>
             <CardMedia
             sx={{width: 500, height: 700}}
-            image = "../static/me.jpg"
+            image = {imgUrl}
             />
         </Card>
     )
